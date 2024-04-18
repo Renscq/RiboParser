208c798450b1a4effe1a376c6ab8f254905043ad
rensc <rensc0718@163.com>
2024-04-18 22:03:32 +0800

    Refactor script name and modified the old version in rpf_Offset_v1.py

--- scripts/rpf_Offset_v1.py
+++ scripts/rpf_Offset_v1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @Project : riboParser
-# @Script  : detect_offset.py
+# @Script  : rpf_Offset_v1.py
 
 
 from foo import ArgsParser
@@ -10,34 +10,41 @@ from foo.Offset_mono import *
 
 def main():
     ArgsParser.now_time()
-    print('Detect the p-site offset.\n', flush=True)
-    print('Step1: Checking the input Arguments.\n', flush=True)
+    sys.stdout.writelines('Detect the p-site offset.\n')
+    sys.stdout.writelines('Step1: Checking the input Arguments.\n')
     args = ArgsParser.offset_args_parser()
     offset_attr = Offset(args)
 
-    print('Step2: Import the transcripts annotation.\n', flush=True)
+    sys.stdout.writelines('Step2: Import the transcripts annotation.\n')
     offset_attr.read_transcript()
 
-    print('Step3: Import the bam file.\n', flush=True)
+    sys.stdout.writelines('Step3: Import the bam file.\n')
     offset_attr.get_mrna_reads()
 
-    print('Step4: Detect the tis offset of sequence profile.\n', flush=True)
+    sys.stdout.writelines('Step4: Detect the offset of sequence profile.\n')
+    if offset_attr.mode == "tis":
+        offset_attr.get_tis_offset()
+        offset_attr.adjust_tis_offset()
 
-    offset_attr.get_tis_offset()
-    offset_attr.adjust_tis_offset()
-    offset_attr.write_tis_offset()
-    offset_attr.draw_tis_heatmap()
+        sys.stdout.writelines('Step5: Output the TIS offset.\n')
+        offset_attr.write_tis_offset()
 
-    print('Step5: Detect the frame offset of sequence profile.\n', flush=True)
-    offset_attr.get_frame_offset()
-    offset_attr.format_frame_offset()
-    offset_attr.adjust_frame_offset()
+        sys.stdout.writelines('Step6: Draw the TIS offset heatmap.\n')
+        offset_attr.draw_tis_heatmap()
 
-    offset_attr.write_frame_offset()
-    offset_attr.draw_frame_heatmap()
+    elif offset_attr.mode == "frame":
+        offset_attr.get_frame_offset()
+        offset_attr.format_frame_offset()
+        offset_attr.adjust_frame_offset()
 
+        sys.stdout.writelines('Step5: Output the frame offset.\n')
+        offset_attr.write_frame_offset()
+
+        sys.stdout.writelines('Step6: Draw the frame offset heatmap.\n')
+        offset_attr.draw_frame_heatmap()
+
+    sys.stdout.writelines('All done.\n')
     ArgsParser.now_time()
-    print('All done.\n', flush=True)
 
 
 if __name__ == '__main__':
