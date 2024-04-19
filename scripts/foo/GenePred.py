cec37c286acbb49984a3ad12821c1d7398a89cd9
rensc <rensc0718@163.com>
2024-04-19 12:58:45 +0800

    Add comments to the functions in the GenePred.py class

--- scripts/foo/GenePred.py
+++ scripts/foo/GenePred.py
@@ -45,6 +45,7 @@ class GenePred(object):
         self.fasta = args.genome
         self.chroms_dict = OrderedDict()
         self.mrna_seq = OrderedDict()
+        self.cds_seq = OrderedDict()
 
         # output file
         self.out_prefix = args.output
@@ -53,9 +54,21 @@ class GenePred(object):
         self.gtf_new = self.out_prefix + '.norm.gtf'
         self.whole_file = self.out_prefix + '.whole.txt'
         self.txt_file = self.out_prefix + '.norm.txt'
-        self.seq_file = self.out_prefix + '.norm.fa'
+        self.rna_file = self.out_prefix + '.norm.rna.fa'
+        self.cds_file = self.out_prefix + '.norm.cds.fa'
 
     def gtf2gp(self):
+        '''
+        @Message  : function to convert the gtf to genepred.
+        @Input    : self.gtf_format --> format of the gtf file
+                    self.gtf --> gtf file
+                    self.gp_file --> genepred file
+
+        @Return   : output --> description
+        @Flow     : step1 --> check the gff3ToGenePred and gtfToGenePred in the current environment
+                    step2 --> convert the gtf or gff to genepred
+        '''
+        
         script_path = os.path.split((os.path.abspath(sys.argv[0])))[0]
         nowpath = os.path.dirname(script_path)
         if self.gtf_format in ['.gff', '.gff3']:
@@ -81,6 +94,15 @@ class GenePred(object):
             sys.exit(1)
 
     def gp2gtf(self):
+        '''
+        @Message  : function to convert the genepred to gtf file.
+        @Input    : self.gp_file --> genepred file
+                    self.gtf_new --> new gtf file
+        @Return   : output --> genePred file
+        @Flow     : step1 --> check the genePredToGtf in the current environment
+                    step2 --> convert the genepred to gtf
+        '''
+        
         # script_path = os.path.split((os.path.abspath(sys.argv[0])))[0]
         # nowpath = os.path.dirname(script_path)
 
@@ -95,6 +117,14 @@ class GenePred(object):
         # os.remove(self.gp_file)
 
     def read_genepred(self):
+        '''
+        @Message  : function import the parser the genepred file .
+        @Input    : self.gp_file --> gene pred file derived from gtf file
+        @Return   : self.gp_df --> dataframe of the genepred file
+        @Flow     : step1 --> read the genepred file and rename the columns
+                    step2 --> calculate the utr5, cds and utr3 length
+        '''
+
         self.gp_df = pd.read_csv(self.gp_file, sep='\t', header=None)
         os.remove(self.gp_file)
         self.gp_df.columns = ['name', 'chrom', 'strand', 'txStart', 'txEnd', 'cdsStart', 'cdsEnd', 'exonCount',
@@ -144,10 +174,12 @@ class GenePred(object):
                     now_right = bisect_right(exonStarts, cdsStart) - 1
                     utr5_intron = sum(introns[0:now_right])
                     utr5_length = cdsStart - exonStarts[0] - utr5_intron
+
                     # get utr3 length
                     now_left = bisect_left(exonEnds, cdsEnd)
                     utr3_intron = sum(introns[now_left:])
                     utr3_length = exonEnds[-1] - cdsEnd - utr3_intron
+
                     # get cds length
                     cds_length = abs(cdsEnd - cdsStart) - intron_length + utr5_intron + utr3_intron
 
@@ -165,10 +197,12 @@ class GenePred(object):
                     now_right = bisect_right(exonStarts, cdsStart) - 1
                     utr3_intron = sum(introns[0:now_right])
                     utr3_length = cdsStart - exonStarts[0] - utr3_intron
+
                     # get utr3 length
                     now_left = bisect_left(exonEnds, cdsEnd)
                     utr5_intron = sum(introns[now_left:])
                     utr5_length = exonEnds[-1] - cdsEnd - utr5_intron
+                    
                     # get cds length
                     cds_length = abs(cdsEnd - cdsStart) - intron_length + utr5_intron + utr3_intron
 
@@ -187,7 +221,18 @@ class GenePred(object):
         self.gp_df['utr3_length'] = utr3_list
 
     def get_rep_transcript_bak(self):
-
+        '''
+        @Message  : function to filter the representative transcripts.
+        @Input    : self.gp_df --> dataframe of the genepred file
+        @Return   : self.gp_df --> dataframe of the genepred file with annotated column [rep_transcript]
+        @Flow     : step1 --> group the dataframe by gene name
+                    step2 --> label the max cds length
+                    step3 --> label the max utr5 + cds + utr3 length
+                    step4 --> label the first transcript
+                    step5 --> add the flag of representative transcript
+        @Note     : low efficiency of this mothod to label the representative transcript.
+        '''
+        
         print('Filter representative transcripts.', flush=True)
 
         counter = 0
@@ -224,6 +269,17 @@ class GenePred(object):
         # print(self.gp_df)
 
     def get_rep_transcript(self):
+        '''
+        @Message  : function to filter the representative transcripts.
+        @Input    : self.gp_df --> dataframe of the genepred file
+        @Return   : self.gp_df --> dataframe of the genepred file with annotated column [rep_transcript]
+        @Flow     : step1 --> calculate the gene length, length = cds_length + utr5_length + utr3_length
+                    step2 --> sort the dataframe by gene name, cds length, gene length, utr5 length and utr3 length
+                    step3 --> reset the index of the dataframe
+                    step4 --> group the dataframe by gene name and label the max cds length and max gene length
+                    step5 --> label the first transcript
+                    step6 --> reset the index of the dataframe
+        '''
 
         print('Filter representative transcripts.', flush=True)
 
@@ -240,21 +296,35 @@ class GenePred(object):
         self.gp_df.reset_index(drop=True, inplace=True)
 
     def add_utr(self):
+        '''
+        @Message  : function to add the pseudo utr.
+        @Input    : self.gp_df --> dataframe of the genepred file
+        @Return   : self.gp_df --> dataframe of the genepred file with annotated column [modified]
+        @Flow     : step1 --> create the list to store the new exon starts and ends
+                    step2 --> skip the transcript without cds
+                    step3 --> calculate the new exon start site to extend the utr5
+                    step4 --> calculate the new exon end site to extend the utr3
+                    step5 --> add the pseudo utr to the transcript
+        '''
+        
         exonStarts_list, exonEnds_list = [], []
         txStart_list, txEnd_list = [], []
         utr5_list, utr3_list = [], []
 
         self.gp_df.loc[:, 'modified'] = 'False'
+
+        # add pseudo utr
         if self.utr == 0:
             pass
-        
+
         else:
-            print('Add pseudo utr.', flush=True)
+            print('Add pseudo UTR.', flush=True)
 
             for idx, rows in self.gp_df.iterrows():
                 if idx % 1000 == 0:
                     print('rows: ' + str(idx), flush=True)
 
+                # skip the transcript without cds
                 if rows['cdsStartStat'] == 'none' or rows['cdsEndStat'] == 'none':
                     exonStarts_list.append(rows.exonStarts)
                     exonEnds_list.append(rows.exonEnds)
@@ -264,43 +334,55 @@ class GenePred(object):
                     utr3_list.append(rows.utr3_length)
                     continue
 
-                # exon starts
+                # check exon starts for each genes
                 exonStarts = rows['exonStarts'].split(',')[:-1]
+                
                 if int(rows['utr5_length']) >= self.utr:
+                    # skip the transcript with utr5 length >= self.utr
                     exonStarts_list.append(rows.exonStarts)
                     txStart_list.append(rows.txStart)
                     utr5_list.append(rows.utr5_length)
 
                 else:
+                    # calculate the new exon start site to extend the utr5
                     new_start = int(exonStarts[0]) - self.utr
+
                     if new_start < 0:
+                        # set the negative new start site to 0, since the chromosome start site can not be negative
                         exonStarts[0] = '0'
                         exonStarts_list.append(','.join(exonStarts) + ',')
                         txStart_list.append(0)
                         utr5_list.append(rows['cdsStart'])
                         self.gp_df.loc[idx, 'modified'] = 'True'
                     else:
+                        # set the new start site to new_start
                         exonStarts[0] = str(new_start)
                         exonStarts_list.append(','.join(exonStarts) + ',')
                         txStart_list.append(rows.txStart - self.utr)
                         utr5_list.append(rows.utr5_length + self.utr)
                         self.gp_df.loc[idx, 'modified'] = 'True'
 
-                # exon ends
+                # check exon ends for each genes
                 exonEnds = rows['exonEnds'].split(',')[:-1]
+
                 if int(rows['utr3_length']) >= self.utr:
+                    # skip the transcript with utr3 length >= self.utr
                     exonEnds_list.append(rows.exonEnds)
                     txEnd_list.append(rows.txEnd)
                     utr3_list.append(rows.utr3_length)
                 else:
+                    # calculate the new exon end site to extend the utr3
                     new_end = int(exonEnds[-1]) + self.utr
+
                     if new_end > self.chroms_dict[str(rows['chrom'])].end:
+                        # set the new end site to chromosome end site, since the chromosome end site can not be larger than the chromosome end site
                         exonEnds[-1] = str(self.chroms_dict[str(rows['chrom'])].end)
                         exonEnds_list.append(','.join(exonEnds) + ',')
                         txEnd_list.append(self.chroms_dict[str(rows['chrom'])].end)
                         utr3_list.append(rows.utr3_length + self.chroms_dict[str(rows['chrom'])].end - rows['cdsEnd'])
                         self.gp_df.loc[idx, 'modified'] = 'True'
                     else:
+                        # set the new end site to new_end
                         exonEnds[-1] = str(new_end)
                         exonEnds_list.append(','.join(exonEnds) + ',')
                         txEnd_list.append(new_end)
@@ -315,37 +397,69 @@ class GenePred(object):
             self.gp_df['utr3_length'] = utr3_list
 
     def read_genome(self):
-
+        '''
+        @Message  : function import the genome sequences.
+        @Input    : self.fasta --> genome file
+        @Return   : self.chroms_dict --> dictionary of the chromosome sequences
+        @Flow     : step1 --> read the genome file with SeqIO
+                    step2 --> store the chromosome sequences in the dictionary
+        '''
+        
         record = SeqIO.parse(self.fasta, "fasta")
+
         for line in record:
             print("import chromosome: {chrom}".format(chrom=line.id), flush=True)
             self.chroms_dict[str(line.id)] = Chrom(line)
 
+
     def get_seq(self):
-        self.mrna_seq = OrderedDict()
+        '''
+        @Message  : function to retrieve the mRNA sequences.
+        @Input    : self.gp_df --> dataframe of the genepred file
+        @Return   : self.mrna_seq --> dictionary of the mRNA sequences
+        @Flow     : step1 --> iterate the dataframe to get the mRNA sequences
+                    step2 --> store the mRNA sequences in the dictionary
+                    step3 --> store the cds sequences in the dictionary
+        '''
 
         for idx, rows in self.gp_df.iterrows():
+
+            # skip the transcript without cds
             if rows['cds_length'] == 0:
                 continue
 
             chromesome = str(rows['chrom'])
             rna = list(zip(rows.exonStarts.split(',')[:-1], rows.exonEnds.split(',')[:-1]))
-            tmp_seq = Seq.Seq('')
+            tmp_rna_seq = Seq.Seq('')
 
             if rows['strand'] == "-":
                 for exon in rna:
                     exon_start, exon_end = int(exon[0]), int(exon[1])
-                    tmp_seq += self.chroms_dict[chromesome].seq[exon_start: exon_end]
-                tmp_seq = tmp_seq.reverse_complement()
+                    tmp_rna_seq += self.chroms_dict[chromesome].seq[exon_start: exon_end]
+                tmp_rna_seq = tmp_rna_seq.reverse_complement()
 
             else:
                 for exon in rna:
                     exon_start, exon_end = int(exon[0]), int(exon[1])
-                    tmp_seq += self.chroms_dict[chromesome].seq[exon_start: exon_end]
+                    tmp_rna_seq += self.chroms_dict[chromesome].seq[exon_start: exon_end]
 
-            self.mrna_seq[rows['name']] = tmp_seq
+            self.mrna_seq[rows['name']] = tmp_rna_seq
+        
+            self.cds_seq[rows['name']] = tmp_rna_seq[rows['utr5_length']: rows['utr5_length'] + rows['cds_length']]
 
     def write_txt(self):
+        '''
+        @Message  : function to output the txt file.
+        @Input    : self.gp_df --> dataframe of the genepred file
+        @Return   : gp_df_new --> dataframe of the genepred file
+                    txt_df --> dataframe of the txt file
+        @Flow     : step1 --> output the whole genepred file
+                    step2 --> filter the coding transcript
+                    step3 --> filter the longest transcript
+                    step4 --> output the genePred file
+                    step5 --> format the genePred file to txt file
+        '''
+        
         if self.whole:
             self.gp_df.to_csv(self.whole_file, sep='\t', header=True, index=None)
 
@@ -375,6 +489,16 @@ class GenePred(object):
         txt_df.to_csv(self.txt_file, sep='\t', header=True, index=None)
 
     def write_seq(self):
-        with open(self.seq_file, 'w') as seq_out:
+        '''
+        @Message  : function to output the mRNA and cds sequences.
+        @Input    : self.rna_file --> mrna sequences file
+                    self.cds_file --> cds sequences file
+        '''
+        
+        with open(self.rna_file, 'w') as seq_out:
             for rna, mess in self.mrna_seq.items():
                 seq_out.writelines('\n'.join(['>' + rna, str(mess)]) + '\n')
+
+        with open(self.cds_file, 'w') as cds_out:
+            for cds, mess in self.cds_seq.items():
+                cds_out.writelines('\n'.join(['>' + cds, str(mess)]) + '\n')
