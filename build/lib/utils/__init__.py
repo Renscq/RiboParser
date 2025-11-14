62de57a5ad68b4e08db373f9b5483282bf842b2c
rensc <rensc0718@163.com>
2025-11-14 19:52:30 +0800

    update version to 0.1.11

--- build/lib/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @time    : 2021/6/11 10:50
-# @Project : riboParser
-# @Script  : __init__.py.py
-# @Version : python 3.8.5
-# @Author  : Rensc
-# @E-mail  : rensc0718@163.com
--- build/lib/utils/ribo/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @time    : 2021/4/19 8:29
-# @Project : riboParser
-# @Author  : Rensc
-# @E-mail  : rensc0718@163.com
